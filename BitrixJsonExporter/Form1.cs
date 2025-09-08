28b94f621e80f0724d8b692dcc7a0730b296242f
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-09-08 11:44:34 +0300

    Добавлено событие изменения статуса экспорта

--- BitrixJsonExporter/Form1.cs
+++ BitrixJsonExporter/Form1.cs
@@ -14,6 +14,7 @@ public partial class Form1 : Form
         _exporter = exporter;
         _httpService = httpService;
         InitializeComponent();
+        _exporter.OnStatusChanged += OnStatusChanged;
     }
 
     private void button2_Click(object sender, EventArgs e)
@@ -57,10 +58,16 @@ public partial class Form1 : Form
             }
             _httpService.ConfigureHttpClient(webhook);
             await _exporter.StartExport(webhook, path);
+            status.Text = "Данные успешно экспортированы";
         }
         catch (Exception ex)
         {
             status.Text = ex.Message;
         }
+    }
+
+    private void OnStatusChanged(string text)
+    {
+        status.Text = text;
     }
 }
--- Business/Interfaces/IExporter.cs
+++ Business/Interfaces/IExporter.cs
@@ -3,4 +3,5 @@ namespace Business.Interfaces;
 public interface IExporter
 {
     Task StartExport(string webHook, string exportPath);
+    event Action<string>? OnStatusChanged;
 }
