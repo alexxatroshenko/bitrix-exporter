72dd24bccf99ff15cdb5d094fb5e7e64facdc082
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-09-09 14:11:27 +0300

    Покрыт метод GetHierarchyList

--- Tests/Business/TasksServiceTests.cs
+++ Tests/Business/TasksServiceTests.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Diagnostics;
 using Business;
 using Business.Interfaces;
 using Data.Interfaces;
@@ -23,21 +24,31 @@ public class TasksServiceTests
         _httpServiceMock = new Mock<IHttpService>();
         _tasksService = new TasksService(_httpServiceMock.Object);
     }
-
-    [Fact]
-    public async Task GetTasksAsync_WhenHttpServiceReturnTasks_ReturnSameTasks()
+    
+    #region TestData
+    public static IEnumerable<object[]> TestDataForGetHierarchyList()
     {
-        var expectedTasks = _tasks;
-
-        _httpServiceMock.Setup(service => service.GetTasksAsync()).ReturnsAsync(expectedTasks);
-
-        var result = await _tasksService.GetTasksAsync();
-
-        result.Should().NotBeNullOrEmpty().And.HaveCount(2).And.BeEquivalentTo(expectedTasks);
+        //Задачи без подзадач
+        var random = new Random();
+        var flatTasks = Enumerable.Range(1, 5)
+            .Select(i => new BitrixTask { Id = i, ParentId = random.Next(2) == 0 ? 0 : null }).ToList();
+        yield return [flatTasks];
         
-        _httpServiceMock.Verify(service => service.GetTasksAsync(), Times.Once);
+        //У задач есть подзадачи
+        var tasksWithSubtasks1 = new List<BitrixTask>
+        {
+            new() { Id = 1, ParentId = 0 },
+            new() { Id = 2, ParentId = 1 },
+            new() { Id = 3, ParentId = 1 },
+            new() { Id = 4, ParentId = 2 },
+            new() { Id = 5, ParentId = 0 },
+            new() { Id = 6, ParentId = 5 },
+            new() { Id = 7, ParentId = 5 },
+            new() { Id = 8, ParentId = 5 },
+        };
+        yield return [tasksWithSubtasks1];
     }
-
+    
     public static IEnumerable<object[]> TestDataForGetComments()
     {
         //Первая и последняя задача имеют комментарии
@@ -61,6 +72,21 @@ public class TasksServiceTests
         var comments = Enumerable.Range(1, 10).Select(i => new List<Comment> { new() {Id = i, PostMessage = $"message {i}"}}).ToList();
         yield return [tasks, comments];
     }
+    #endregion
+    
+    [Fact]
+    public async Task GetTasksAsync_WhenHttpServiceReturnTasks_ReturnSameTasks()
+    {
+        var expectedTasks = _tasks;
+
+        _httpServiceMock.Setup(service => service.GetTasksAsync()).ReturnsAsync(expectedTasks);
+
+        var result = await _tasksService.GetTasksAsync();
+
+        result.Should().NotBeNullOrEmpty().And.HaveCount(2).And.BeEquivalentTo(expectedTasks);
+        
+        _httpServiceMock.Verify(service => service.GetTasksAsync(), Times.Once);
+    }
     
     [Theory]
     [MemberData(nameof(TestDataForGetComments))]
@@ -79,6 +105,35 @@ public class TasksServiceTests
         {
             result[i].Comments.Should().HaveCount(serviceCommentsOutput.Count > i ? serviceCommentsOutput[i].Count : 0).And
                 .BeEquivalentTo(serviceCommentsOutput.Count > i ? serviceCommentsOutput[i] : []);
+        }
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestDataForGetHierarchyList))]
+    public void GetHierarchyList_ShouldBuildCorrectHierarchy(List<BitrixTask> inputTasks)
+    {
+        var hierarchy = _tasksService.GetHierarchyList(inputTasks);
+        var rootTasks = inputTasks.Where(x => x.ParentId is null or 0).ToList();
+        
+        hierarchy.Should().HaveCount(rootTasks.Count);
+
+        AssertHierarchy(rootTasks, inputTasks);
+    }
+    
+    private static void AssertHierarchy(List<BitrixTask> tasks, List<BitrixTask> inputTasks)
+    {
+        foreach (var task in tasks)
+        {
+            task.ChildTasks
+                .Should()
+                .HaveCount(inputTasks.Count(x => x.ParentId == task.Id));
+            
+            Debug.WriteLine($"task {task.Id}");
+            
+            if (task.ChildTasks.Count != 0)
+            {
+                AssertHierarchy(task.ChildTasks, inputTasks);
+            }
         }
     }
 }
